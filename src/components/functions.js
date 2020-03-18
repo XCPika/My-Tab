e95df903c9c2324a421b3f8963bcd27a51c1ddba
XCPika <pixel_games@icloud.com>
2020-03-18 15:20:25 +0000

    Changes to goTo function

--- src/components/functions.js
+++ src/components/functions.js
@@ -13,7 +13,7 @@ const getCatagory = (value) => {
 };
 
 const goTo = (site) => {
-    window.location.href = "//www.google.com/search?q="+site
+    window.location.href = site
 }
 
 export { getTitle, getCatagory, goTo } 
